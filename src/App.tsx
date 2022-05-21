8b42347d8f1076e7e0d78c6b6426148c9f6217cf
Barash777 <barash.vertihvost@gmail.com>
2022-05-22 02:08:26 +0300

    lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,30 @@ import './App.css';
 function App() {
     return (
         <div className="App">
+            <Header/>
             Hello, samurai! Let's go!
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-            </ul>
+            <List/>
         </div>
     );
+}
+
+const Header = () => {
+    return <div>
+        <a href={"#"}>Home </a>
+        <a href={"#"}>Cafe </a>
+        <a href={"#"}>Restaurant </a>
+    </div>
+}
+
+const List = () => {
+    return <div>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </div>
 }
 
 export default App;
