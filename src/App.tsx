db34eec177431d07e85c9da972df49f0619a3eec
Barash777 <barash.vertihvost@gmail.com>
2022-05-22 00:25:23 +0300

    lesson 6

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello, samurai! Let's go!
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
